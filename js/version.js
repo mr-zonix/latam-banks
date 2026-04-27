e956a8445619284c00b16868efe98eedbbe6129b
Mr Zonix <mrzonix@github.com>
2026-04-27 22:51:08 +0000

    Add ISV Ecosystem section with Temenos verified clients
    - New /isv/ section with ISV ecosystem mapping
    - Temenos page with 7 verified LATAM clients:
      - Diners Club Ecuador (press release Feb 2023)
      - Bancolombia (Wikipedia)
      - Banco do Brasil (Wikipedia)
      - Banco Bradesco (Wikipedia)
      - BBVA (Wikipedia)
      - Banco Santander (Wikipedia)
    - Updated sidebar navigation with ISV link
    - Dashboard version 4.4

--- js/version.js
+++ js/version.js
@@ -1,7 +1,6 @@
 const DASHBOARD_VERSION = {
     version: "4.4",
     date: "Apr 2026",
-    lastUpdate: "2026-04-16",
-    author: "Mr Zonix",
-    changes: "Standardized executive buttons across all 27 bank pages"
+    lastUpdate: "2026-04-27",
+    author: "Mr Zonix"
 };
